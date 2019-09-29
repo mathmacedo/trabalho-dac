2bd53d2659557175814ab00033de1e4a8524b2bd
sauxthem <matt.rock4321@hotmail.com>
2019-09-29 15:42:22 -0300

    git add menu_gerente, CRUD funcionarios. add managedBeans to show success messages on register. minor improvements

---
+++ dac_final_gerencial/src/java/com/tads/dac/managedBeans/funcionarioBean.java
@@ -0,0 +1,29 @@
+package com.tads.dac.managedBeans;
+
+import java.io.IOException;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+@Named(value = "funcionarioBean")
+@RequestScoped
+public class funcionarioBean {
+    private String nome;
+    
+    public funcionarioBean() {}
+
+    public String getNome() { return nome; } 
+    public void setNome(String nome) { this.nome = nome; }
+    
+    public void cadastrar() throws IOException{
+        FacesContext.getCurrentInstance().addMessage(null, new
+            FacesMessage(FacesMessage.SEVERITY_INFO, "Cadastro Realizado!", null));
+        
+        FacesContext.getCurrentInstance().getExternalContext()
+            .getFlash().setKeepMessages(true);
+        
+        FacesContext.getCurrentInstance()
+                .getExternalContext().redirect("pesquisar_funcionarios.xhtml"); 
+    }
+}
---
+++ dac_final_gerencial/web/js/pesquisar_funcionarios.js
@@ -0,0 +1,6 @@
+function confirmarExclusao(){
+    if(confirm("Tem certeza que deseja excluir esse funcionario?"))
+        alert("Funcionário excluído!");
+}
+
+
