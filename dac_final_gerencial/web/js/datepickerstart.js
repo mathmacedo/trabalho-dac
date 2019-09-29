a2261b82851f1e0e101461718ee0e08269d6a8c7
Matheus Ribeiro <matt.rock4321@hotmail.com>
2019-09-29 12:40:58 -0300

    add gerencial index, consultar_assentos/voos, efetuar_chekin, menu_funcionario. improvements to index.css

---
+++ dac_final_gerencial/web/js/datepickerstart.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  var inicio=$('input[name="j_idt9:inicio"]'); 
+  inicio.datepicker({
+    format: 'dd/mm/yyyy',
+    todayHighlight: true,
+    autoclose: true
+  });
+  
+  var final=$('input[name="j_idt9:final"]'); 
+    final.datepicker({
+    format: 'dd/mm/yyyy',
+    todayHighlight: true,
+    autoclose: true
+  })
+  
+});
