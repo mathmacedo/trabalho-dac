4aa672995915c8251a277934052c686138921825
Matheus Ribeiro <matt.rock4321@hotmail.com>
2019-09-29 14:26:56 -0300

    add checkinBean. implement efetuar_checkin. new css styles for jsf messages

---
+++ dac_final_gerencial/src/java/com/tads/dac/managedBeans/checkinBean.java
@@ -0,0 +1,22 @@
+package com.tads.dac.managedBeans;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+@Named(value = "checkinBean")
+@RequestScoped
+public class checkinBean {
+    private String checkin;
+    
+    public checkinBean() {}
+    
+    public String getCheckin(){ return this.checkin; }
+    public void setCheckin(String s){ this.checkin = s; }
+    
+    public void efetuarCheckin() {
+        FacesContext.getCurrentInstance().addMessage(null, new
+        FacesMessage(FacesMessage.SEVERITY_INFO, "Checkin realizado!", null));
+    }
+}
