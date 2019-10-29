4645b5fb24ee0155aa666c7c143dd7892cd0ba64
sauxthem <matt.rock4321@hotmail.com>
2019-10-29 20:41:49 -0300

    Add cidadeDAO

---
+++ dac_final_gerencial/src/java/com/tads/dac/dao/CidadeDAO.java
@@ -0,0 +1,103 @@
+package com.tads.dac.dao;
+
+import com.tads.dac.beans.Cidade;
+import com.tads.dac.util.HibernateUtil;
+import java.io.Serializable;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class CidadeDAO {
+    public int insertCidade(Cidade cidade){
+        try{
+            Session s = HibernateUtil.getSessionFactory().openSession();
+            Transaction t = s.beginTransaction();
+
+            Serializable idCidade = s.save(cidade);
+            t.commit();
+
+            s.close();
+
+            return Integer.parseInt(idCidade.toString());
+        }
+        catch(Exception ex){
+            throw new RuntimeException(
+                "Erro ao inserir cidade: " + ex.getMessage());
+        }
+    }
+    
+    public Cidade getCidadeById(int id){
+        try{
+            Session s = HibernateUtil.getSessionFactory().openSession();
+            Transaction t = s.beginTransaction();
+            
+            Cidade c = (Cidade) s.get(Cidade.class, id);
+            
+            t.commit();
+            s.close();
+            
+            return c;
+        }
+        catch(Exception ex){
+            throw new RuntimeException(
+                "Erro ao buscar cidade: " + ex.getMessage());
+        }
+    }
+    
+    public List<Cidade> listCidades(){
+        try{
+            Session s = HibernateUtil.getSessionFactory().openSession();
+            Transaction t = s.beginTransaction();
+            Query q = s.createQuery("from Cidade");
+            
+            List<Cidade> lista = q.list();
+            t.commit();
+            s.close();
+            
+            return lista;
+        }
+        catch(Exception ex){
+            throw new RuntimeException(
+                "Erro ao listar cidades: " + ex.getMessage());
+        }
+    }
+    
+    public boolean updateCidade(Cidade cidade){
+        try{
+            Session s = HibernateUtil.getSessionFactory().openSession();
+            Transaction t = s.beginTransaction();
+            
+            s.update(cidade);
+            
+            t.commit();
+            s.close();
+            
+            return true;
+        }
+        catch(Exception ex){
+            throw new RuntimeException(
+                "Erro ao atualizar cidade: " + ex.getMessage());
+        }
+    }
+    
+    public boolean deleteCidade(int id){
+        try{
+            Session s = HibernateUtil.getSessionFactory().openSession();
+            Transaction t = s.beginTransaction();
+            
+            Cidade c = (Cidade) s.get(Cidade.class, id);
+            s.update(c.getEstado());
+            s.delete(c);
+            
+            t.commit();
+            s.close();
+            
+            return true;
+        }
+        catch(Exception ex){
+            throw new RuntimeException(
+                "Erro ao deletar cidade: " + ex.getMessage());
+        }
+    }
+}
