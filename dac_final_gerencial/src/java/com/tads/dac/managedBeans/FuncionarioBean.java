10a7a9837fe05fec819ced97ff79175102847543
sauxthem <matt.rock4321@hotmail.com>
2019-11-12 20:09:59 -0300

    Changes on funcionario pages and MB to implement Funcionario insertion

--- dac_final_gerencial/src/java/com/tads/dac/managedBeans/FuncionarioBean.java
+++ dac_final_gerencial/src/java/com/tads/dac/managedBeans/FuncionarioBean.java
@@ -1,11 +1,15 @@
 package com.tads.dac.managedBeans;
 
 import com.tads.dac.beans.Cidade;
+import com.tads.dac.beans.Endereco;
 import com.tads.dac.beans.Estado;
 import com.tads.dac.beans.Funcionario;
 import com.tads.dac.facade.CidadeFacade;
+import com.tads.dac.facade.EnderecoFacade;
 import com.tads.dac.facade.EstadoFacade;
+import com.tads.dac.facade.FuncionarioFacade;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
@@ -16,7 +20,8 @@ import javax.inject.Named;
 @Named(value = "funcionarioBean")
 @RequestScoped
 public class FuncionarioBean {
-    private Funcionario funcionario;
+    private Funcionario funcionario;    
+    private List<Funcionario> funcionarios;
     private List<Estado> listaEstados;
     private List<Cidade> listaCidades;
     private Cidade cidade;
@@ -26,6 +31,7 @@ public class FuncionarioBean {
     private String email;
     private char cargo;
     private String senha;
+    private String confSenha;
     private String bairro;
     private String rua;
     private int numero;
@@ -35,11 +41,28 @@ public class FuncionarioBean {
     @PostConstruct
     public void init(){
         this.funcionario = new Funcionario();
+        this.funcionarios = FuncionarioFacade.listFuncionarios();
         this.listaEstados = EstadoFacade.listEstados();
         this.estado = listaEstados.get(0);
         this.listaCidades = CidadeFacade.getCidadesByEstado(estado.getId());
     }
 
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    public List<Funcionario> getFuncionarios() {
+        return funcionarios;
+    }
+
+    public void setFuncionarios(List<Funcionario> funcionarios) {
+        this.funcionarios = funcionarios;
+    }
+    
     public List<Estado> getListaEstados() {
         return listaEstados;
     }
@@ -112,6 +135,14 @@ public class FuncionarioBean {
         this.senha = senha;
     }
 
+    public String getConfSenha(){ 
+        return this.confSenha; 
+    }
+    
+    public void setConfSenha(String senha){
+        this.confSenha = senha;
+    }
+    
     public String getBairro() {
         return bairro;
     }
@@ -156,16 +187,68 @@ public class FuncionarioBean {
         this.listaCidades = CidadeFacade.getCidadesByEstado(this.estado.getId());
     }
     
-    public void cadastrar() throws IOException{
+    public void cadastrar() throws IOException, NoSuchAlgorithmException{
         Funcionario f = new Funcionario();
+        Endereco e = new Endereco();
         
-        FacesContext.getCurrentInstance().addMessage(null, new
-            FacesMessage(FacesMessage.SEVERITY_INFO, "Cadastro Realizado!", null));
+        if (senha.equals(confSenha))
+            f.setSenha(senha);
+        else {
+            FacesContext.getCurrentInstance().addMessage(null, new
+            FacesMessage(FacesMessage.SEVERITY_ERROR, "Senhas não coincidem!", null));
         
-        FacesContext.getCurrentInstance().getExternalContext()
-            .getFlash().setKeepMessages(true);
+            FacesContext.getCurrentInstance().getExternalContext()
+                .getFlash().setKeepMessages(true);
+
+            FacesContext.getCurrentInstance()
+                    .getExternalContext().redirect("pesquisar_funcionarios.xhtml"); 
+        }
         
-        FacesContext.getCurrentInstance()
-                .getExternalContext().redirect("pesquisar_funcionarios.xhtml"); 
+        e.setCidade(cidade);
+        e.setComplemento(complemento);
+        e.setNumero(numero);
+        e.setRua(rua);
+        
+        int idEndereco = EnderecoFacade.insertEndereco(e);
+        if (idEndereco != 0) e.setId(idEndereco);
+        else {
+            FacesContext.getCurrentInstance().addMessage(null, new
+            FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro ao inserir endereço!", null));
+        
+            FacesContext.getCurrentInstance().getExternalContext()
+                .getFlash().setKeepMessages(true);
+
+            FacesContext.getCurrentInstance()
+                    .getExternalContext().redirect("pesquisar_funcionarios.xhtml"); 
+        }
+        
+        f.setEndereco(e);
+        f.setCpf(cpf);
+        f.setEmail(email);
+        f.setNome(nome);
+        f.setTipo(cargo);
+        
+        int idFuncionario = FuncionarioFacade.insertFuncionario(f);
+        if (idFuncionario != 0) {
+            FacesContext.getCurrentInstance().addMessage(null, new
+                FacesMessage(FacesMessage.SEVERITY_INFO, "Cadastro Realizado!", null));
+
+            FacesContext.getCurrentInstance().getExternalContext()
+                .getFlash().setKeepMessages(true);
+
+            FacesContext.getCurrentInstance()
+                    .getExternalContext().redirect("pesquisar_funcionarios.xhtml"); 
+        }
+        
+        else {
+            FacesContext.getCurrentInstance().addMessage(null, new
+                FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro ao cadastrar funcionário!", null));
+
+            FacesContext.getCurrentInstance().getExternalContext()
+                .getFlash().setKeepMessages(true);
+
+            FacesContext.getCurrentInstance()
+                    .getExternalContext().redirect("pesquisar_funcionarios.xhtml"); 
+        }
     }
 }
