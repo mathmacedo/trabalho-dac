fcd288245c3bf214c3e4720b00be1f1ac0bafd09
sauxthem <matt.rock4321@hotmail.com>
2019-11-23 15:10:50 -0300

    Feat/CRUD - Changes to implement all CRUD operations for CidadeAeroporto

--- dac_final_gerencial/src/java/com/tads/dac/managedBeans/CidadeBean.java
+++ dac_final_gerencial/src/java/com/tads/dac/managedBeans/CidadeBean.java
@@ -1,52 +1,217 @@
 package com.tads.dac.managedBeans;
 
+import com.tads.dac.beans.Cidade;
+import com.tads.dac.beans.CidadeAeroporto;
+import com.tads.dac.beans.Estado;
+import com.tads.dac.facade.CidadeAeroportoFacade;
+import com.tads.dac.facade.CidadeFacade;
+import com.tads.dac.facade.EstadoFacade;
 import java.io.IOException;
-import javax.enterprise.context.RequestScoped;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 @Named(value = "cidadeBean")
-@RequestScoped
-public class CidadeBean {
+@SessionScoped
+public class CidadeBean implements Serializable {
     private int id;
     private String nome;
-    private EstadoBean estado;
+    private String sigla;
+    private Estado estado;
+    private Cidade cidade;
+    private char method;
+    private List<CidadeAeroporto> listaAeroportos;
+    private List<Cidade> listaCidades;
+    private List<Estado> listaEstados;
     
     public CidadeBean() {}
 
-    public int getId(){ return this.id; }
-    public void setId(int id){
-        if (id > 0) this.id = id;
-        else throw new RuntimeException(
-            "Erro: Id da cidade não pode ser nulo!");
+    @PostConstruct
+    public void init(){
+        this.listaAeroportos = CidadeAeroportoFacade.listCidadeAeroportos();
+        this.listaCidades = CidadeFacade.listCidades();
+        this.listaEstados = EstadoFacade.listEstados();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+
+    public char getMethod() {
+        return method;
+    }
+
+    public void setMethod(char method) {
+        this.method = method;
+    }
+
+    public List<CidadeAeroporto> getListaAeroportos() {
+        return listaAeroportos;
+    }
+
+    public void setListaAeroportos(List<CidadeAeroporto> listaAeroportos) {
+        this.listaAeroportos = listaAeroportos;
+    }
+
+    public List<Cidade> getListaCidades() {
+        return listaCidades;
+    }
+
+    public void setListaCidades(List<Cidade> listaCidades) {
+        this.listaCidades = listaCidades;
+    }
+
+    public List<Estado> getListaEstados() {
+        return listaEstados;
+    }
+
+    public void setListaEstados(List<Estado> listaEstados) {
+        this.listaEstados = listaEstados;
     }
     
-    public String getNome() { return nome; } 
-    public void setNome(String nome) { 
-        if (nome != null)
-            this.nome = nome; 
-        else throw new RuntimeException(
-            "Erro: Nome da Cidade não pode ser nulo!");
+    public void buscarCidades(){
+        this.listaCidades = CidadeFacade.getCidadesByEstado(this.estado.getId());
     }
     
-    public EstadoBean getEstado(){ return this.estado; }
-    public void setEstado(EstadoBean estado){
-        if (estado != null && estado.getId() > 0 && estado.getNome() != null
-                && estado.getSigla() != null) this.estado = estado;
-        else throw new RuntimeException(
-            "Erro: Estado inválido na criação de Cidade!");
-    }
-    
-    public void cadastrar() throws IOException{
-        FacesContext.getCurrentInstance().addMessage(null, new
+    public String cadastrar() throws IOException{
+        CidadeAeroporto c = new CidadeAeroporto();
+        c.setCidade(cidade);
+        c.setNome(nome);
+        c.setSigla(sigla);
+        
+        int idAeroporto = CidadeAeroportoFacade.insertCidadeAeroporto(c);
+        
+        if (idAeroporto > 0){
+            FacesContext.getCurrentInstance().addMessage(null, new
             FacesMessage(FacesMessage.SEVERITY_INFO, 
-                    "Cidade cadastrada com sucesso!", null));
+                    "Aeroporto cadastrado com sucesso!", null));
         
-        FacesContext.getCurrentInstance().getExternalContext()
-            .getFlash().setKeepMessages(true);
+            FacesContext.getCurrentInstance().getExternalContext()
+                .getFlash().setKeepMessages(true);
+
+            this.listaAeroportos = CidadeAeroportoFacade.listCidadeAeroportos();
+            
+            return "pesquisar_cidades";
+        }
+        else{
+            FacesContext.getCurrentInstance().addMessage(null, new
+            FacesMessage(FacesMessage.SEVERITY_ERROR, 
+                    "Erro ao cadastrar Aeroporto!", null));
         
-        FacesContext.getCurrentInstance()
-                .getExternalContext().redirect("pesquisar_cidades.xhtml"); 
+            FacesContext.getCurrentInstance().getExternalContext()
+                .getFlash().setKeepMessages(true);
+
+            return "pesquisar_cidades";
+        }
+    }
+    
+    public String atualizar(){
+        CidadeAeroporto c = CidadeAeroportoFacade.getCidadeAeroportoById(id);
+        
+        c.setCidade(cidade);
+        c.setNome(nome);
+        c.setSigla(sigla);
+        
+        if(CidadeAeroportoFacade.updateCidadeAeroporto(c)){
+            this.listaAeroportos = CidadeAeroportoFacade.listCidadeAeroportos();
+            
+            FacesContext.getCurrentInstance().addMessage(null, new
+            FacesMessage(FacesMessage.SEVERITY_INFO, 
+                    "Aeroporto alterado com sucesso!!", null));
+        
+            FacesContext.getCurrentInstance().getExternalContext()
+                .getFlash().setKeepMessages(true);
+
+            return "pesquisar_cidades";
+        }
+        else{
+            FacesContext.getCurrentInstance().addMessage(null, new
+                FacesMessage(FacesMessage.SEVERITY_ERROR, 
+                    "Erro ao atualizar Aeroporto!", null));
+        
+            FacesContext.getCurrentInstance().getExternalContext()
+                .getFlash().setKeepMessages(true);
+
+            return "manter_cidades";
+        }
+    }
+    
+    public void remover(int id){
+        if (CidadeAeroportoFacade.deleteCidadeAeroporto(id)){
+            FacesContext.getCurrentInstance().addMessage(null, new
+                FacesMessage(FacesMessage.SEVERITY_INFO, 
+                    "Aeroporto removido com sucesso!", null));
+            this.listaAeroportos = CidadeAeroportoFacade.listCidadeAeroportos();
+        }
+        else{
+            FacesContext.getCurrentInstance().addMessage(null, new
+                FacesMessage(FacesMessage.SEVERITY_ERROR, 
+                    "Erro ao remover Aeroporto!", null));
+        }
+    }
+    
+    public String openView(CidadeAeroporto aero, char method){
+        clearData();
+        
+        this.method = method;
+        
+        if (method != 'n'){
+            this.id = aero.getId();
+            this.cidade = aero.getCidade();
+            this.estado = aero.getCidade().getEstado();
+            this.nome = aero.getNome();
+            this.sigla = aero.getSigla();
+        }
+        
+        return "manter_cidades";
+    }
+    
+    public void clearData(){
+        this.cidade = null;
+        this.estado = null;
+        this.nome = null;
+        this.id = 0;
+        this.sigla = null;
+        this.method = 0;
     }
 }
