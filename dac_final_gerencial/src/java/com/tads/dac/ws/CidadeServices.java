9dbf613b2e7d8c4d51f3c66cbcc5c9fd5e5dc5c8
danditeoa <taci.dandi@gmail.com>
2019-11-28 23:30:15 -0300

    feat(cidades-webservice): add get cidadeBu id estado

--- dac_final_gerencial/src/java/com/tads/dac/ws/CidadeServices.java
+++ dac_final_gerencial/src/java/com/tads/dac/ws/CidadeServices.java
@@ -7,6 +7,7 @@ package com.tads.dac.ws;
 
 import com.tads.dac.beans.Cidade;
 import com.tads.dac.facade.CidadeFacade;
+import java.util.List;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
@@ -14,7 +15,9 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+
 
 /**
  * REST Web Service
@@ -31,8 +34,17 @@ public class CidadeServices {
     }
 
     @GET
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Cidade getCidade() {
-        return CidadeFacade.getCidadeById(1);
+    public List<Cidade> getCidade() {
+        return CidadeFacade.listCidades();
+    }
+    
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+    public List<Cidade> getCidadeById(@PathParam("id") int id) {
+       return CidadeFacade.getCidadesByEstado(id);
     }
 }
