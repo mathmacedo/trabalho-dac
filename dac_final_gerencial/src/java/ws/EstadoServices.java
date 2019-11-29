5c3512f75229a69f32996faf16519c35697fc3e4
danditeoa <taci.dandi@gmail.com>
2019-11-29 01:31:38 -0300

    feat(endereco):create service

---
+++ dac_final_gerencial/src/java/ws/EstadoServices.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws;
+
+import com.tads.dac.facade.EstadoFacade;
+import com.tads.dac.beans.Estado;
+import java.util.List;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("/estado")
+public class EstadoServices {
+
+    @Context
+    private UriInfo context;
+
+    public EstadoServices() {
+    }
+
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Estado> getEstados() {
+        return EstadoFacade.listEstados();
+    }
+}
