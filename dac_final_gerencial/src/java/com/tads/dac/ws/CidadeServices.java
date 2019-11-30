5d7900eccf5b7c7a796ce914762111292a1fbb2a
sauxthem <matt.rock4321@hotmail.com>
2019-11-30 19:50:47 -0300

    adding ws to this branch, actually no idea what im doing

---
+++ dac_final_gerencial/src/java/com/tads/dac/ws/CidadeServices.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tads.dac.ws;
+
+import com.tads.dac.beans.Cidade;
+import com.tads.dac.facade.CidadeFacade;
+import java.util.List;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("/cidade")
+public class CidadeServices {
+
+    @Context
+    private UriInfo context;
+
+    public CidadeServices() {
+    }
+
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Cidade> getCidade() {
+        return CidadeFacade.listCidades();
+    }
+    
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+    public List<Cidade> getCidadeById(@PathParam("id") int id) {
+       return CidadeFacade.getCidadesByEstado(id);
+    }
+}
