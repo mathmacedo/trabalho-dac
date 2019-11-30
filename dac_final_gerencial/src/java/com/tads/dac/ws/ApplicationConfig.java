3a1762308663ac6098f96fc5c0b0d2f377cfbfbf
danditeoa <taci.dandi@gmail.com>
2019-11-30 20:43:33 -0300

    scaffold-listvoos

--- dac_final_gerencial/src/java/com/tads/dac/ws/ApplicationConfig.java
+++ dac_final_gerencial/src/java/com/tads/dac/ws/ApplicationConfig.java
@@ -30,6 +30,7 @@ public class ApplicationConfig extends Application {
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(com.tads.dac.ws.CidadeServices.class);
+        resources.add(com.tads.dac.ws.ListVoos.class);
     }
     
 }
